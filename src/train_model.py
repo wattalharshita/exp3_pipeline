ee82090d3c0d04f0274307f27e331563eab7bd0f
harshita <wattalharshita@gmail.com>
2026-03-10 09:24:13 +0530

    Update train_model.py

--- src/train_model.py
+++ src/train_model.py
@@ -1,6 +1,7 @@
 from sklearn.linear_model import LinearRegression
 import pandas as pd
 import joblib
+import os
 
 data = pd.read_csv("data/housing.csv")
 
@@ -10,6 +11,7 @@ y = data["price"]
 model = LinearRegression()
 model.fit(X, y)
 
+os.makedirs("models", exist_ok=True)
 joblib.dump(model, "models/housing_model.pkl")
 
 print("Model trained and saved successfully")
